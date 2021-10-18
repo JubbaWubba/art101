2230098e15c2d9fd452af82ecd9dc978a5ce957f
JubbaWubba <gabrielmaghanoyr@gmail.com>
2021-10-18 16:56:54 -0700

    lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,19 @@
+ // *
+ // *
+ // *
+ // **Author: Gabriel Rivera
+ // // Created: 10/18/21
+
+ make = "Toyota";
+ model = "Prius";
+ color = "black";
+ year = "2001";
+ ownit = false;
+ age = 2021 - year;
+
+ document.writeln("Make: " + make + "<br>");
+ document.writeln("Model: " + model + "<br>");
+ document.writeln("Color: " + color + "<br>");
+ document.writeln("Year: " + year + "<br>");
+ document.writeln("Age: " + age + "<br>");
+ document.writeln("Ownership: " + ownit + "<br>");
