f81bb78cda336e93e0782a846299fed795b22321
JubbaWubba <gabrielmaghanoyr@gmail.com>
2021-10-20 18:17:49 -0500

    lab 6

---
+++ lab6/js/lab.js
@@ -0,0 +1,22 @@
+// *
+// *
+// *
+// **Author: Gabriel Rivera
+// // Created: 10/20/21
+
+//define variables
+myTransport = ["walk","Uber","Lyft","getting driven by a friend"];
+// defining main myMainRide
+var myMainRide = {
+   make: "Toyota",
+   model: "Prius",
+   color: "black",
+   year: "2001",
+   ownit: false,
+   age: function() {
+     return 2021 - age;
+   }
+}
+//output
+document.writeln("Kinds of transportation I use: ", myTransport, "</br>");
+document.writeln("My main ride: <pre>", JSON.stringify(myMainRide, null, '\t'), "</pre>");
