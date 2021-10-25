2f8570829f5a66b07a9dc0c61fb696bae566ed3a
JubbaWubba <gabrielmaghanoyr@gmail.com>
2021-10-25 16:49:37 -0700

    lab7 pt 2

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -3,22 +3,28 @@
 // *
 // **Author: Gabriel Rivera
 // // Created: 10/25/21
-function username (); {
-  // Window Prompt Popup, get name
-  var name = window.prompt("Hello, What is your name?");
-  console.log("name =", name);
-  // Name into an array
+var name = window.prompt("Hello, What is your name?");
+console.log("name =", name);
+
+function username (name) {
+
+  // Name into an array, called userarray
   var userarray = name.split('');
   console.log("Array =", userarray);
-  //Sort the array
+  //Sort the array, userarray gets turned into userarraysorted
   var userarraysorted = userarray.sort();
   console.log("Sorted Array =", userarraysorted);
-  //join
+  //join, userarraysorted gets turned into newname
   var newname = userarraysorted.join('');
   console.log("new name =", newname);
 
   return newname;
 }
 
+//Output
+function output () {
+  document.writeln("Your name was: ", name, "</br>");
+  document.writeln("Your name is now: ", username(name), "</br>");
+}
 
-document.writeln("Your name is now: ", username(), "</br>");
+output();
